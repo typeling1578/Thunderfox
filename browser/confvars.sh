d11333e5d7a29f45afa9f15cffbad63450965389
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-06-01 19:40:00 +0900

    Update build config.

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -5,6 +5,7 @@
 
 MOZ_APP_VENDOR=typeling1578
 MOZ_APP_NAME=thunderfox
+MOZ_DISTRIBUTION_ID=org.thunderfox
 MOZ_APP_UA_NAME=Firefox
 
 if test "$OS_ARCH" = "WINNT"; then
@@ -43,3 +44,7 @@ MOZ_PROFILE_MIGRATOR=1
 
 # Include the DevTools client, not just the server (which is the default)
 MOZ_DEVTOOLS=all
+
+# Thunderfox
+MOZ_DATA_REPORTING=
+MOZ_TELEMETRY_REPORTING=
