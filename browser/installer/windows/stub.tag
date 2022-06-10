8e3461d68a339124d84f599925d91e5fa88fe572
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-06-10 23:57:47 +0900

    Rename Firefox references to Thunderfox

--- browser/installer/windows/stub.tag
+++ browser/installer/windows/stub.tag
@@ -1,4 +1,4 @@
 ;!@Install@!UTF-8!
-Title="Mozilla Firefox"
+Title="Thunderfox"
 RunProgram="setup-stub.exe"
 ;!@InstallEnd@!
--- toolkit/xre/PolicyChecks.h
+++ toolkit/xre/PolicyChecks.h
@@ -11,7 +11,7 @@
 
 #  include <windows.h>
 
-#  define POLICY_REGKEY_NAME L"SOFTWARE\\Policies\\Mozilla\\" MOZ_APP_BASENAME
+#  define POLICY_REGKEY_NAME L"SOFTWARE\\Policies\\typeling1578\\" MOZ_APP_BASENAME
 
 // NB: This code must be able to run apart from XPCOM
 
