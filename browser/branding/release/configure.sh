c1d68c6ebd1dee8d18c83cd55a69e3fe2fe85363
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-06-18 13:36:08 +0900

    Add branding

---
+++ browser/branding/release/configure.sh
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_DISPLAYNAME=Thunderfox
+
+MOZ_HANDLER_CLSID="5c838d05-ad77-5653-9d2e-4c80ad235ec4"
+MOZ_IHANDLERCONTROL_IID="ce532d2b-402a-5d65-bf0a-a5b87f3ce67e"
+MOZ_ASYNCIHANDLERCONTROL_IID="484b4074-363f-5f68-8ebc-8d0b74620e0f"
+MOZ_IGECKOBACKCHANNEL_IID="9a4691b8-bcb1-54b4-8bf7-b40321c32776"
