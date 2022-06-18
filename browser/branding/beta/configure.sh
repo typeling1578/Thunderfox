5df358c7475e18e57b25eecaae05618fe9ccab31
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-06-18 13:36:08 +0900

    Add branding

---
+++ browser/branding/beta/configure.sh
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_DISPLAYNAME="Thunderfox Beta"
+
+MOZ_HANDLER_CLSID="1f378382-23c9-59dd-a683-1fd8e96e5be0"
+MOZ_IHANDLERCONTROL_IID="4b71d471-f509-577f-8bc8-c713547ea048"
+MOZ_ASYNCIHANDLERCONTROL_IID="81d756f1-fec3-5459-8cab-81460a588198"
+MOZ_IGECKOBACKCHANNEL_IID="1288472d-18d1-5470-9ca5-b73b4ed0168c"
