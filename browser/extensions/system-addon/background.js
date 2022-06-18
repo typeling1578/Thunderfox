245ad2c5b2265e1655af0e796bfec0aed2f2866d
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-06-18 13:45:42 +0900

    Enhanced privacy.

--- browser/extensions/system-addon/background.js
+++ browser/extensions/system-addon/background.js
@@ -3,6 +3,7 @@ browser.storage.local.get({
 }, async function(options) {
     if (options.firstStart) {
         await browser.browserLangSettings.setAppLangToSystemLang();
+        await browser.aboutConfigPrefs.setCharPref("browser.contentblocking.category", "strict");
         browser.storage.local.set({
             firstStart: false
         })
