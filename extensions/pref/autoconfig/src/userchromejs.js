e2553447d12041d9ad0de41577ec3b5251b38129
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-06-25 21:23:18 +0900

    Enable Legacy Add-ons

--- extensions/pref/autoconfig/src/userchromejs.js
+++ extensions/pref/autoconfig/src/userchromejs.js
@@ -1,7 +1,6 @@
 // skip 1st line
 try {
-  // Legacy add-on support is currently disabled due to remaining security concerns.
-  // Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
+  Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
 } catch (ex) {};
 
 try {
