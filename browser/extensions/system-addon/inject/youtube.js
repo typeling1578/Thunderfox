a9a1240a161ea1947447e66b8ed63d8f5af19b8c
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-07-02 18:07:15 +0900

    Modify variable scope.

--- browser/extensions/system-addon/inject/youtube.js
+++ browser/extensions/system-addon/inject/youtube.js
@@ -1,21 +1,23 @@
-function inject() {
-    setInterval(function () {
-        window["_lact"] = Date.now();
-    }, 300000);
-
-    if (location.hostname == "www.youtube.com") {
-        let elem = null;
+(function () {
+    function inject() {
         setInterval(function () {
-            if(!elem) {
-                elem = document.querySelector('ytd-watch-flexy');
-            }
-            if (elem) {
-                elem.youthereDataChanged_ = function () { };
-                elem.youThereManager_.youThereData_ = null;
-            }
-        }, 2000)
+            window["_lact"] = Date.now();
+        }, 300000);
+
+        if (location.hostname == "www.youtube.com") {
+            let elem = null;
+            setInterval(function () {
+                if (!elem) {
+                    elem = document.querySelector('ytd-watch-flexy');
+                }
+                if (elem) {
+                    elem.youthereDataChanged_ = function () { };
+                    elem.youThereManager_.youThereData_ = null;
+                }
+            }, 2000)
+        }
     }
-}
-let elem = document.createElement("script");
-elem.innerText = "(" + inject.toString() + "())";
-document.documentElement.appendChild(elem)
+    let elem = document.createElement("script");
+    elem.innerText = "(" + inject.toString() + "())";
+    document.documentElement.appendChild(elem)
+}())
