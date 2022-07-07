9f7c21d4da7f55b717a8a9bb69aceb793d18dea9
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-07-07 16:09:08 +0900

    Legacy features have been disabled by default.

--- extensions/pref/autoconfig/src/userchromejs.js
+++ extensions/pref/autoconfig/src/userchromejs.js
@@ -1,8 +1,16 @@
 // skip 1st line
-try {
-  Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
-} catch (ex) {};
+{
+  const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
-try {
-  Cu.import('chrome://userchromejs/content/userChrome.jsm');
-} catch (ex) {};
+  try {
+    if (Services.prefs.getBoolPref("extensions.experiments.legacyAddons.enabled", false)) {
+      Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
+    }
+  } catch (ex) {};
+
+  try {
+    if (Services.prefs.getBoolPref("toolkit.legacyUserProfileCustomizations.javascript", false)) {
+      Cu.import('chrome://userchromejs/content/userChrome.jsm');
+    }
+  } catch (ex) {};
+}
