5dcfc90d87d010bcf23da16ae9376c11afd9867f
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-07-09 02:15:28 +0900

    Fixed a "bug" on YouTube where playback would stop after a period of time.

---
+++ browser/extensions/system-addon/inject/youtube.js
@@ -0,0 +1,23 @@
+(function () {
+    function inject() {
+        setInterval(function () {
+            window["_lact"] = Date.now();
+        }, 300000);
+
+        if (location.hostname == "www.youtube.com") {
+            let elem = null;
+            setInterval(function () {
+                if (!elem) {
+                    elem = document.querySelector('ytd-watch-flexy');
+                }
+                if (elem) {
+                    elem.youthereDataChanged_ = function () { };
+                    elem.youThereManager_.youThereData_ = null;
+                }
+            }, 2000)
+        }
+    }
+    let elem = document.createElement("script");
+    elem.innerText = "(" + inject.toString() + "())";
+    document.documentElement.appendChild(elem)
+}())
