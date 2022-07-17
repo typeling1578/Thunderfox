3ff525fd8c5b78f9ffcc804ee974e34fcf80b9be
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-07-17 19:30:45 +0900

    The default font for Japanese on Windows has been changed to Noto Sans JP.

--- browser/app/profile/0-thunderfox-prefs.js
+++ browser/app/profile/0-thunderfox-prefs.js
@@ -94,3 +94,7 @@ pref("privacy.restrict3rdpartystorage.rollout.enabledByDefault", true);
 
 pref("app.feedback.baseURL", "https://github.com/typeling1578/Thunderfox/issues");
 pref("extensions.getAddons.discovery.api_url", "https://addons.mozilla.org/api/v4/accounts/account/17138352/collections/Add-ons-for-Thunderfox/addons/?sort=-popularity&lang=%LOCALE%");
+
+#ifdef XP_WIN
+  pref("font.name-list.sans-serif.ja", "Noto Sans JP, Meiryo, Yu Gothic, MS PGothic, MS Gothic, Yu Mincho, MS PMincho, MS Mincho");
+#endif // XP_WIN
